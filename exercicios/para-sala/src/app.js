96bf73788f65af2159945c23d82575555c084a17
nearlyerika <72031082+nearlyerika@users.noreply.github.com>
2023-05-20 16:45:31 -0300

    aula feita

--- exercicios/para-sala/src/app.js
+++ exercicios/para-sala/src/app.js
@@ -2,9 +2,9 @@ const express = require("express") //importando o express
 const app = express() // executo o express
 app.use(express.json()) // uso o bodyparser
 
+const filmesRoutes = require('./routes/filmesRoutes')
 
-
-
+app.use('./filmes')
 
 // exportando para usar o server.js
 module.exports = app 
---
+++ exercicios/para-sala/src/controllers/filmesController.js
@@ -0,0 +1,81 @@
+const filmesJson = require('../models/filmes.json')
+const express = require('express')
+const app = express()
+
+//
+
+const getALL = (req, res) => {
+
+    res.status(200).json([{
+        'filmes': filmesJson
+    }])
+}
+
+//
+
+const updateMovieByID = (req, res) => {
+
+    const idRequest = req.params.id
+    let movieRequest = req.body
+    let foundMovie = filmesJson.findIndex(movie => movie.id === idRequest)
+    filmesJson.splice(foundMovie, 1, movieRequest)
+
+    res.status(200).json([{
+        'message': 'updated movie',
+        filmesJson
+    }])
+}
+
+//
+
+const updateTitleByID = (req, res) => {
+
+    const idRequest = req.params.id
+    let newTitle = req.body.title
+    let foundMovie = filmesJson.findIndex(movie => movie.id === idRequest)
+    foundMovie.title = newTitle
+
+    res.status(200).json([{
+        'message': 'updated title',
+        foundMovie
+    }])
+}
+
+//
+
+const deleteByID = (req, res) => {
+
+    const idRequest = req.params.id
+    const deletedMovie = filmesJson.findIndex(movie => movie.id === idRequest)
+    filmes.Json.splice(deleteMovie, 1)
+
+    res.status(200).json([{
+        "message": "Movie deleted",
+        "deleted": idRequest,
+        filmesJson
+    }])
+}
+
+//
+
+const deleteByTitle = (req, res) => {
+    const idRequest = req.params.title.toLowerCase()
+    const deleteMovie = filmesJson.findIndex(movie => movie.title.toLocaleLowerCase() === idRequest)
+    filmesJson.splice(deleteMovie, 1)
+
+    res.status(200).json([{
+        "message": "Movie deleted",
+        "deleted": idRequest,
+        filmesJson
+    }])
+}
+
+//
+
+module.exports = {
+    getALL,
+    updateMovieByID,
+    updateTitleByID,
+    deleteByID,
+    deleteByTitle
+}
