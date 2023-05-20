967ac2f7187d970918e0e3729e754e5b9c39ff6c
Nazaré Almeida <126839545+nazare4lmeida@users.noreply.github.com>
2023-05-20 16:51:20 -0300

    Alterações em aula

--- exercicios/para-sala/src/app.js
+++ exercicios/para-sala/src/app.js
@@ -2,8 +2,9 @@ const express = require("express") //importando o express
 const app = express() // executo o express
 app.use(express.json()) // uso o bodyparser
 
+const filmesRoutes = require("./routes/filmesRoutes") //importando os filmes do routes
 
-
+app.use("/filmes", filmesRoutes) //criei uma rota raiz
 
 
 // exportando para usar o server.js
