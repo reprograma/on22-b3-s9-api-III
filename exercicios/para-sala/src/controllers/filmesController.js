47ff1202e8b336037ddda9be339c83500ba3ed94
Ângela Oliveira <89156781+anjinha-oliveira@users.noreply.github.com>
2023-05-20 16:08:45 -0300

    Adicionando rotas get, put,  patch, delete

--- exercicios/para-sala/src/controllers/filmesController.js
+++ exercicios/para-sala/src/controllers/filmesController.js
@@ -9,6 +9,51 @@ const getAll = (req, res) => {
     }])
 }
 
+const updateMovieByID = (req, res) => {
+    const IdRequest = req.params.id
+    let movieRequest = req.body 
+    let findMovie = filmesJson.findIndex(movie => movie.id == IdRequest )
+// Está removendo 1 elemento, a partir do index encontrado e adicionando o movieRequest no lugar 
+    filmesJson.splice(findMovie, 1, movieRequest)
+    res.status(200).json([{
+        'message': 'updated movie/filme atualizado',
+        filmesJson
+    }])
+}
+
+//declarando minha constante de atualizar titulo do filme:
+
+const updateTitleByID = (req, res) => {
+    const IdRequest = req.params.id //Defino que vou buscar pelo ID pois o ID é unico
+    let newTitle = req.body.title //Altero o titulo pelo body através do ID
+    let findMovie = filmesJson.find(movie => movie.id == IdRequest) //filtrei o meu 'banco de dados' para encontrar o id 
+
+    //depois disso, no meu filme, a parte especifica do titulo sera alterada pelo novo titulo
+    findMovie.title = newTitle
+    res.status(200).json([{
+        'message': 'Titulo do filme atualizado/Update title',
+        findMovie
+    }])
+}
+
+const deleteMovieByID = (req, res) => {
+    const IdRequest = req.params.id
+    const deleteMovie = filmesJson.findIndex(movie => movie.id == IdRequest)
+    filmesJson.splice(deleteMovie, 1)
+
+    res.status(200).json([{
+        'message': 'Deleted Movie',
+        'Deleted': IdRequest,
+        filmesJson
+    }])
+}
+
+
 module.exports = {
     getAll,
+    updateMovieByID,
+    updateTitleByID,
+    deleteMovieByID
 }
+
+
--- exercicios/para-sala/src/routes/filmesRoutes.js
+++ exercicios/para-sala/src/routes/filmesRoutes.js
@@ -4,4 +4,27 @@ const express = require("express")
 const router = express.Router()
 
 router.get('/catalogo', filmesController.getAll)
+router.put('/update/:id', filmesController.updateMovieByID)
+router.patch('/change/:id', filmesController.updateTitleByID)
+router.delete('/delete/:id', filmesController.deleteMovieByID)
+
+
 module.exports = router
+
+
+
+
+
+
+
+
+
+
+
+
+//Por onde começar as configurações:
+//n1: server.js
+//n2: app.js
+//n3: models => Quando utilizar banco de dados
+//n4: routes = Definir rotas pode ajudar na logica depois
+//n5: controllers = Logica de programação das rotas
