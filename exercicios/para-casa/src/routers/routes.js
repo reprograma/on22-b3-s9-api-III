ffec0ebf946d33ac4adc22eca4d8c43823039359
nearlyerika <72031082+nearlyerika@users.noreply.github.com>
2023-05-23 13:49:12 -0300

    changed file names

--- exercicios/para-casa/src/routers/routes.js
+++ exercicios/para-casa/src/routers/routes.js
@@ -1,19 +1,23 @@
+// Imports /////////////////////////////////////////////////////
 const express = require('express')
-const movieController = require('../controllers/controllers.js')
+const movieController = require('../controllers/controller.js')
 const router = express.Router()
 
+// Routes //////////////////////////////////////////////////////
+// GETs
 router.get('/catalogo', movieController.getALL)
-router.get('/catalogo/:id', movieController.getByID)
-
+router.get('/catalogo/:id', movieController.getMovieByID)
+// PUTs
 router.put('/update/:id', movieController.updateMovieByID)
 router.put('/update/:id', movieController.updateDescriptionByID)
-
+// PATCHEs
 router.patch('/change/:id', movieController.updateTitleByID)
 router.patch('/change/:id', movieController.updateDirectorByID)
 router.patch('/change/:id', movieController.updateRunningTimeByID)
-
+// DELETEs
 router.delete('/delete/:id', movieController.deleteByID)
 router.delete('/delete/:title', movieController.deleteByTitle)
 router.delete('/delete/:producer', movieController.deleteByProducer)
 
+// Exports /////////////////////////////////////////////////////
 module.exports = router
