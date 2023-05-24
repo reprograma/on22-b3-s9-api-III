c38359356f792ab91dd57a078d69f53a2c1e23af
Ângela Oliveira <89156781+anjinha-oliveira@users.noreply.github.com>
2023-05-24 10:24:12 -0300

    Adicionando logica e rotas para exibir todos os filmes no postman

--- exercicios/para-casa/src/controllers/controllerFilms.js
+++ exercicios/para-casa/src/controllers/controllerFilms.js
@@ -2,3 +2,16 @@ const FilmsJson = require('../models/ghiblifilms.json')
 const express = require('express')
 const app = express()
 app.use(express.json())
+
+//Exibir todos os filmes
+const getAll = (request, response) => {
+    response.status(200).json([{
+        'filmes': FilmsJson
+    }])
+}
+
+
+
+module.exports = {
+    getAll
+}
--- exercicios/para-casa/src/routes/routesFilms.js
+++ exercicios/para-casa/src/routes/routesFilms.js
@@ -2,6 +2,8 @@ const filmsController = require('../controllers/controllerFilms')
 const express = require('express')
 const router = express.Router()
 
+router.get('/filmes', filmsController.getAll) //exibindo todos os filmes
+
 
 
 
