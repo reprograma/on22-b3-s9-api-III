9a12dc3ee264c6810197c38cd5b2a8812cfda346
Ângela Oliveira <89156781+anjinha-oliveira@users.noreply.github.com>
2023-05-24 17:09:58 -0300

    adicionando controller de update description

--- exercicios/para-casa/src/controllers/controllerFilms.js
+++ exercicios/para-casa/src/controllers/controllerFilms.js
@@ -3,7 +3,6 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
-//Exibir todos os filmes
 const getAll = (request, response) => {
     response.status(200).json([{
         'filmes': FilmsJson
@@ -47,6 +46,7 @@ const updateDescriptionById = (request, response) => {
     }])
 
 }
+
 
 
 module.exports = {
