bcf5bad3c9753a2da951184ef971b5889d9d33ac
Cilene <cilene.kinomoto@gmail.com>
2023-05-30 19:43:18 -0300

    Exercício de casa finalizado_Jucilene Bratti

---
+++ exercicios/para-casa/src/app.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+
+const ghibliRoutes = require("./routes/ghibliRoutes")
+
+app.use("/ghibli", ghibliRoutes)
+
+
+module.exports = app
