38c7f4bae10d81aed3d179f620fbf417376b7aba
nadiacastilho <ncastilhopersonal@gmail.com>
2023-08-01 17:36:37 +0100

    9

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-const app = require("./exercicios/para-sala/src/app") //chamando o arquivo app
-
+//chamando o arquivo app
+const app = require("./exercicios/para-casa/src/app")
 const PORT = 1313 //PORTA
 
 //iniciando o servidor
 app.listen(PORT , ()=>{
-    console.log(`To rodando na porta ${PORT}`)
-}) 
+    console.log(`To rodando na porta ${PORT}`) })
